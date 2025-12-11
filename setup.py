52472250a0ccb5f7d11e9fbe97bfeddff5651e04
TheAditya-10 <aditya.pratap.singh.tomar.1082006@gmail.com>
2025-12-12 03:55:35 +0530

    Working Directory setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The Future Has Signals. We Decode Them.',
+    author='Aditya Pratap Singh Tomar',
+    license='MIT',
+)
